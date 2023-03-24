f0c09686af6f24cc2e8a86bdd62d52d0e2c695e4
PedroC01 <p2201cardoso360@hotmail.com>
2023-03-24 01:22:27 +0000

    UI color Swap on bars,Messing With Cameras Again, and VFX

--- Assets/Scripts/MedaHealthSlider.cs
+++ Assets/Scripts/MedaHealthSlider.cs
@@ -9,24 +9,48 @@ public class MedaHealthSlider : MonoBehaviour
     public GameObject medapart;
     public float medaPartEnergy;
     public Slider partEnergySlider;
+    public float percentageValue;
+    public Gradient gradient;
+    public float maxEnergy;
+    public Image fill;
     void Start()
     {
         medaPartEnergy= medapart.GetComponent<MedaPartScript>().partEnergy;
+        maxEnergy = medaPartEnergy;
         this.partEnergySlider=GetComponent<Slider>();
         this.partEnergySlider.value = medapart.GetComponent<MedaPartScript>().partEnergy;
+       fill.color= gradient.Evaluate(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         //  SetEnergy();
+      
         this.partEnergySlider.value = medapart.GetComponent<MedaPartScript>().partEnergy;
+        percentageValue = (this.partEnergySlider.value * 100) / maxEnergy;
+        if (65 >= percentageValue && percentageValue >= 36)
+        {
+            fill.color = this.gradient.Evaluate(0.5f);
+        }
+        if (35 >= percentageValue)
+        {
+            fill.color = this.gradient.Evaluate(0f);
+        }
     }
    
 
     public void SetEnergy() {
 
         this.partEnergySlider.value = medapart.GetComponent<MedaPartScript>().partEnergy;
-       
+        percentageValue = (this.partEnergySlider.value * 100) / maxEnergy;
+        if (65 >= percentageValue && percentageValue >= 36)
+        {
+            fill.color=this.gradient.Evaluate(0.5f);
+        }
+        if (35 >= percentageValue)
+        {
+            fill.color=this.gradient.Evaluate(0f);
+        }
     }
 }
