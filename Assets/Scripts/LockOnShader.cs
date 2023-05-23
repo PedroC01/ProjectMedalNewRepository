662072da29d9a59af264a7ef588b988d43f6b29f
PedroC01 <p2201cardoso360@hotmail.com>
2023-05-23 03:22:23 +0100

    FixingMetabee,CreatingTexturesMetabee,WorkingOnLockOnShaderScipt

--- Assets/Scripts/LockOnShader.cs
+++ Assets/Scripts/LockOnShader.cs
@@ -1,35 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class LockOnShader : MonoBehaviour
 {
 
-    [SerializeField]
-    public UnityEvent LockOnSh;
+    
     private LockOn LO;
     public int thisPieceNum;
-    public int thisLockOnNum;
+    public static int thisLockOnNum;
     private GameObject thisMedapartParent;
     public GameObject[] thisPartPieces;
+    new GameObject[] singlePartpieces;
     public GameObject Enemy;
-    public Material ShaderMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
         if (this.gameObject.GetComponentInParent<Player2>() == true)
         {
             this.Enemy = FindObjectOfType<Player1>().gameObject;
-            this.Enemy.GetComponent<LockOn>();
+            this.LO=this.Enemy.GetComponent<LockOn>();
             this.thisPartPieces = GameObject.FindGameObjectsWithTag("Player2PartPiece");
         
         }
         if (this.gameObject.GetComponentInParent<Player1>() == true)
         {
             this.Enemy = FindObjectOfType<Player2>().gameObject;
-            this.Enemy.GetComponent<LockOn>();
+            this.LO=this.Enemy.GetComponent<LockOn>();
             this.thisPartPieces = GameObject.FindGameObjectsWithTag("Player1PartPiece");
        
          
@@ -44,6 +44,8 @@ public class LockOnShader : MonoBehaviour
             }
 
         }
+        thisLockOnNum = LO.pieceReference;
+        ChangePieceEffect();
 
     }
     // Update is called once per frame
@@ -56,23 +58,28 @@ public class LockOnShader : MonoBehaviour
 
     public void ChangePieceEffect()
     {
-        this.thisLockOnNum = LO.pieceReference;
+        thisLockOnNum = LO.pieceReference;
         foreach (GameObject piece in thisPartPieces)
         {
-            if (piece.GetComponent<PartPiece>().PieceNum==thisLockOnNum)
+            if (thisLockOnNum == thisPieceNum)
             {
-
-                piece.GetComponentInChildren<SkinnedMeshRenderer>().material = ShaderMaterial;
-
+                piece.GetComponentInChildren<PartPiece>().isLocked = true;
             }
             else
             {
-           //  piece.GetComponentInChildren<SkinnedMeshRenderer>().material.;
+                piece.GetComponentInChildren<PartPiece>().isLocked = false;
             }
+        
+       piece.GetComponentInChildren<PartPiece>().ChangeMaterial();
 
-        }
+       }
+         
+
     }
 
 
+ }
 
-}
+
+
+
--- Assets/Scripts/PartPiece.cs
+++ Assets/Scripts/PartPiece.cs
@@ -1,13 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PartPiece : MonoBehaviour
 {
     public int PieceNum;
-    void Start()
+    [SerializeField]
+     Material loShader;
+    [SerializeField]
+     Material thisPeaceMaterial;
+    public bool isLocked;
+    [SerializeField]
+    public UnityEvent LockOnSh;
+    [SerializeField]
+    public UnityEvent Base;
+    private SkinnedMeshRenderer renderer1;
+
+    private void Start()
     {
-      
+      renderer1 = GetComponent<SkinnedMeshRenderer>();
+        
+    }
+    public void ChangeMaterial()
+    {
+        
+        if (this.isLocked == false)
+        {
+          //  this.renderer1.material= thisPeaceMaterial;
+            Base.Invoke();
+        }
+        if(this.isLocked == true)
+        {
+           // renderer1.material= loShader;
+            LockOnSh.Invoke();
+        }
     }
 
     
