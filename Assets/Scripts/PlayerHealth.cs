6af00cb0ae6ade4d25941aca0f497874b9450f26
PedroC01 <p2201cardoso360@hotmail.com>
2023-06-18 03:15:59 +0100

    TryingAnimationLayers

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -9,31 +11,53 @@ public class PlayerHealth : MonoBehaviour
     private MedaPartScript mScript;
     public float partsTotalHealth;
     public GameObject Enemy;
-    public float eastDamage;
-    public float northDamage;
-
+    float averageHealth;
+    public GameObject HeadTorsoHealth;
+    public float HeadHealth;
+    public float lifePerToMedaforce;
+    public bool canGoBerserk=false;//Can use MedaForce
     // Start is called before the first frame update
     void Start()
     {
 
         if (this.gameObject.GetComponent<Player2>()==true)
         {
-            Enemy = FindObjectOfType<Player1>().gameObject;
+            this.MedaParts = GameObject.FindGameObjectsWithTag("Player2Parts");
+        }
+        if (this.gameObject.GetComponent<Player1>() == true)
+        {
+            this.MedaParts = GameObject.FindGameObjectsWithTag("Player1Parts");
         }
 
-        MedaParts = GameObject.FindGameObjectsWithTag("Player2Parts");
-            //Find<MedaPartScript>(mScript);
-        foreach (var part in MedaParts)
+        //Find<MedaPartScript>(mScript);
+        foreach (var part in this.MedaParts)
         {
             mScript = GetComponent<MedaPartScript>();
             partsTotalHealth += part.GetComponent<MedaPartScript>().partEnergy;
+            if (mScript.MedapartNumber == 1)
+            {
+                this.HeadTorsoHealth = part;
+            }
         }
-      
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        ////terminar o jogo
+        HeadHealth = HeadTorsoHealth.GetComponent<MedaPartScript>().partEnergy;
+        if (HeadHealth <= 0)
+        {
+
+        }
+
+        //ativar medaforce se a media da vida passar x
+        averageHealth = partsTotalHealth / (MedaParts.Length);
+        if (averageHealth < lifePerToMedaforce)
+        {
+            canGoBerserk = true;
+        }
         
     }
 }
