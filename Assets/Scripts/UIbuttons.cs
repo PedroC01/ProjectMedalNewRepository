825451f56d3137e2077ffaa2ba4020711f55931f
PedroC01 <p2201cardoso360@hotmail.com>
2023-07-07 23:29:41 +0100

    UIAlmost

--- Assets/Scripts/UIbuttons.cs
+++ Assets/Scripts/UIbuttons.cs
@@ -7,15 +7,14 @@ using UnityEngine.UI;
 public class UIbuttons : MonoBehaviour
 {
     public int UINumber;
-    private Vector3 initialScale = new Vector3(1, 1, 1);
-    public RectTransform coverRectTransformEast;
-    public RectTransform coverRectTransformWest;
-    public RectTransform coverRectTransformRocket; 
-    public RectTransform coverRectTransformDash;
+    public GameObject coverObjectEast;
+    public GameObject coverObjectWest;
+    public GameObject coverObjectRocket;
+    public GameObject coverObjectDash;
     public int timerEast; // Timer for recharge East
     public int timerWest; // Timer for recharge West
     public int timerRocket;
-    public int timerDash;// Timer for recharge Rocket
+    public int timerDash; // Timer for recharge Rocket
     public float rechargeTimeEast; // Recharge time for East ability
     public float rechargeTimeWest; // Recharge time for West ability
     public float rechargeTimeRocket; // Recharge time for Rocket ability
@@ -23,22 +22,20 @@ public class UIbuttons : MonoBehaviour
     private Shooter sh;
     private RocketLaucher RL;
     private PlayerMovements pm;
-    public float maxScale = 1f;
-    public float minScale = 0f;
     [SerializeField]
     public TMP_Text eastTimeText; // TMP_Text for East ability time
     [SerializeField]
     public TMP_Text westTimeText; // TMP_Text for West ability time
     public TMP_Text rocketTimeText; // TMP_Text for Rocket ability time
     public TMP_Text dashTimeText;
-    private float eastScale = 0f; // Current scale value for East ability
-    private float westScale = 0f; // Current scale value for West ability
-    private float rocketScale = 0f; // Current scale value for Rocket ability
-    private float dashScale = 0f;
-    private float eastScaleVelocity = 0f; // Velocity for smoothing East ability scale
-    private float westScaleVelocity = 0f; // Velocity for smoothing West ability scale
-    private float rocketScaleVelocity = 0f; // Velocity for smoothing Rocket ability scale
-    private float dashScaleVelocity = 0f;
+    private float eastRechargePercentage = 0f; // Current recharge percentage for East ability
+    private float westRechargePercentage = 0f; // Current recharge percentage for West ability
+    private float rocketRechargePercentage = 0f; // Current recharge percentage for Rocket ability
+    private float dashRechargePercentage = 0f;
+    public Slider eastSlider; // Slider for East ability
+    public Slider westSlider; // Slider for West ability
+    public Slider rocketSlider; // Slider for Rocket ability
+    public Slider dashSlider; // Slider for Dash ability
 
     [Header("Sounds")]
     public string attackReady;
@@ -51,6 +48,7 @@ public class UIbuttons : MonoBehaviour
     private bool isAttackReadyB = false;
     private bool isAttackReadyC = false;
     private bool isDashReady = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -69,144 +67,86 @@ public class UIbuttons : MonoBehaviour
         attackReadySoundInstanceEast = FMODUnity.RuntimeManager.CreateInstance(attackReady);
         attackReadySoundInstanceWest = FMODUnity.RuntimeManager.CreateInstance(attackReady);
         attackReadySoundInstanceRocket = FMODUnity.RuntimeManager.CreateInstance(attackReady);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rechargePercentageEast = Mathf.Clamp01(this.sh.TimerForRechargeEast / rechargeTimeEast);
-        float rechargePercentageWest = Mathf.Clamp01(this.sh.TimerForRechargeWest / rechargeTimeWest);
-        float rechargePercentageRocket = Mathf.Clamp01(this.RL.TimerForRecharge / rechargeTimeRocket); // Get Rocket recharge percentage
-        float rechargePercentDash = Mathf.Clamp01(this.pm.dashCoolDown / rechargeTimeDash);
         // Handle Recharge East
-        if (rechargePercentageEast > 0f)
-        {
-            eastScale = Mathf.SmoothDamp(eastScale, maxScale, ref eastScaleVelocity, 0.2f);
-            coverRectTransformEast.localScale = Vector3.Lerp(Vector3.zero, initialScale, eastScale);
-            timerEast = (int)sh.TimerForRechargeEast; // Update the timer for recharge East
-            eastTimeText.text = timerEast.ToString();
-            eastTimeText.gameObject.SetActive(timerEast > 0);
+        eastRechargePercentage = Mathf.Clamp01(this.sh.TimerForRechargeEast / rechargeTimeEast);
+        timerEast = (int)sh.TimerForRechargeEast; // Update the timer for recharge East
+        eastTimeText.text = timerEast.ToString();
+        eastTimeText.gameObject.SetActive(timerEast > 0);
+        eastSlider.maxValue = rechargeTimeEast;
+        eastSlider.value = sh.TimerForRechargeEast;
 
-            if (!isAttackReadyA && rechargePercentageEast >= 1f)
-            {
-                isAttackReadyA = true;
-               // attackReadySoundInstanceEast.start();
-            }
-            
+        if (sh.TimerForRechargeEast <= 0f)
+        {
+            coverObjectEast.SetActive(false);
+            eastSlider.gameObject.SetActive(false);
         }
         else
         {
-            eastScale = Mathf.SmoothDamp(eastScale, minScale, ref eastScaleVelocity, 0.2f);
-            coverRectTransformEast.localScale = Vector3.Lerp(Vector3.zero, initialScale, eastScale);
-
-            if (sh.TimerForRechargeEast == 0f)
-            {
-                if (isAttackReadyA)
-                {
-                    attackReadySoundInstanceEast.start(); // Play the sound when attack becomes ready
-                }
-                isAttackReadyA = false;
-                eastTimeText.gameObject.SetActive(timerEast > 0);
-            }
+            coverObjectEast.SetActive(true);
+            eastSlider.gameObject.SetActive(true);
         }
 
         // Handle Recharge West
-        if (rechargePercentageWest > 0f)
-        {
-            westScale = Mathf.SmoothDamp(westScale, maxScale, ref westScaleVelocity, 0.2f);
-            coverRectTransformWest.localScale = Vector3.Lerp(Vector3.zero, initialScale, westScale);
-            timerWest = (int)sh.TimerForRechargeWest;
-            westTimeText.text = timerWest.ToString();
-            westTimeText.gameObject.SetActive(timerWest > 0);
+        westRechargePercentage = Mathf.Clamp01(this.sh.TimerForRechargeWest / rechargeTimeWest);
+        timerWest = (int)sh.TimerForRechargeWest;
+        westTimeText.text = timerWest.ToString();
+        westTimeText.gameObject.SetActive(timerWest > 0);
+        westSlider.maxValue = rechargeTimeWest;
+        westSlider.value = sh.TimerForRechargeWest;
 
-            if (!isAttackReadyB && rechargePercentageWest >= 1f)
-            {
-                isAttackReadyB = true;
-               // attackReadySoundInstanceWest.start();
-            }
-           
+        if (sh.TimerForRechargeWest <= 0f)
+        {
+            coverObjectWest.SetActive(false);
+            westSlider.gameObject.SetActive(false);
         }
         else
         {
-            westScale = Mathf.SmoothDamp(westScale, minScale, ref westScaleVelocity, 0.2f);
-            coverRectTransformWest.localScale = Vector3.Lerp(Vector3.zero, initialScale, westScale);
-
-            if (sh.TimerForRechargeWest == 0f)
-            {
-                if (isAttackReadyB)
-                {
-                   attackReadySoundInstanceWest.start(); // Play the sound when attack becomes ready
-                }
-                isAttackReadyB = false;
-                westTimeText.gameObject.SetActive(timerWest > 0);
-            }
+            coverObjectWest.SetActive(true);
+            westSlider.gameObject.SetActive(true);
         }
 
         // Handle Recharge Rocket
-        if (rechargePercentageRocket > 0f)
-        {
-            rocketScale = Mathf.SmoothDamp(rocketScale, maxScale, ref rocketScaleVelocity, 0.2f);
-            coverRectTransformRocket.localScale = Vector3.Lerp(Vector3.zero, initialScale, rocketScale);
-            timerRocket = (int)RL.TimerForRecharge;
-            rocketTimeText.text = timerRocket.ToString();
-            rocketTimeText.gameObject.SetActive(timerRocket > 0);
+        rocketRechargePercentage = Mathf.Clamp01(this.RL.TimerForRecharge / rechargeTimeRocket);
+        timerRocket = (int)RL.TimerForRecharge;
+        rocketTimeText.text = timerRocket.ToString();
+        rocketTimeText.gameObject.SetActive(timerRocket > 0);
+        rocketSlider.maxValue = rechargeTimeRocket;
+        rocketSlider.value = RL.TimerForRecharge;
 
-            if (!isAttackReadyC && rechargePercentageRocket >= 1f)
-            {
-                isAttackReadyC = true;
-               // attackReadySoundInstanceRocket.start();
-            }
-           
+        if (RL.TimerForRecharge <= 0f)
+        {
+            coverObjectRocket.SetActive(false);
+            rocketSlider.gameObject.SetActive(false);
         }
         else
         {
-            rocketScale = Mathf.SmoothDamp(rocketScale, minScale, ref rocketScaleVelocity, 0.2f);
-            coverRectTransformRocket.localScale = Vector3.Lerp(Vector3.zero, initialScale, rocketScale);
-
-            if (RL.TimerForRecharge == 0f)
-            {
-                if (isAttackReadyC)
-                {
-                    attackReadySoundInstanceRocket.start(); // Play the sound when attack becomes ready
-                }
-                isAttackReadyC = false;
-                rocketTimeText.gameObject.SetActive(timerRocket > 0);
-            }
+            coverObjectRocket.SetActive(true);
+            rocketSlider.gameObject.SetActive(true);
         }
 
+        // Handle Recharge Dash
+        dashRechargePercentage = Mathf.Clamp01(pm.dashCoolDown / rechargeTimeDash);
+        timerDash = (int)pm.dashCoolDown;
+        dashTimeText.text = timerDash.ToString();
+        dashTimeText.gameObject.SetActive(timerDash > 0);
+        dashSlider.maxValue = rechargeTimeDash;
+        dashSlider.value = pm.dashCoolDown;
 
-        //change this to dash
-        if (rechargePercentDash > 0f)
+        if (pm.dashCoolDown <= 0f)
         {
-            dashScale = Mathf.SmoothDamp(dashScale, maxScale, ref dashScaleVelocity, 0.2f);
-            coverRectTransformDash.localScale = Vector3.Lerp(Vector3.zero, initialScale, dashScale);
-            timerDash = (int)pm.dashCoolDown;
-            dashTimeText.text = timerDash.ToString();
-            dashTimeText.gameObject.SetActive(timerDash > 0);
-
-            if (!isDashReady && rechargePercentDash >= 1f)
-            {
-                isDashReady = true;
-                
-            }
-
+            coverObjectDash.SetActive(false);
+            dashSlider.gameObject.SetActive(false);
         }
         else
         {
-            dashScale = Mathf.SmoothDamp(dashScale, minScale, ref dashScaleVelocity, 0.2f);
-            coverRectTransformDash.localScale = Vector3.Lerp(Vector3.zero, initialScale, dashScale);
-
-            if (pm.dashCoolDown == 0f)
-            {
-                if (isDashReady)
-                {
-                                                    // Play the sound when dash becomes ready
-                }
-                isDashReady = false;
-               dashTimeText.gameObject.SetActive(timerDash > 0);
-            }
+            coverObjectDash.SetActive(true);
+            dashSlider.gameObject.SetActive(true);
         }
     }
 }
-
-
